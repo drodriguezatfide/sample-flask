638ebbcdd9ab81888b52e2b1cfad275fd699c52e
David Rodríguez <drodriguez@fidelizador.com>
2024-07-04 16:02:44 -0400

    Add redis

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+import redis 
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    print("hola")
+
+    client = redis.StrictRedis(host='redis-cluster', port=6379, decode_responses=True)
+    try: 
+        client.ping()
+        print("Connected to Redis")
+
+    except Exception:
+        print("Error connecting to Redis")
+
     return "Hello from Python!"
 
 if __name__ == "__main__":
