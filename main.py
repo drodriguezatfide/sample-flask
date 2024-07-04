3742b67b68926b0e7bf5669f9d67448137b22fd4
David Rodríguez <drodriguez@fidelizador.com>
2024-07-04 17:09:59 -0400

    Change redis host

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 def hello():
     app.logger.info("hola")
 
-    client = redis.StrictRedis(host='redis-cluster', port=6379, decode_responses=True)
+    client = redis.StrictRedis(host='10.43.239.42', port=6379, decode_responses=True)
     try: 
         client.ping()
         app.logger.info("Connected to Redis")
 
-    except Exception:
+    except Exception as e:
         app.logger.info("Error connecting to Redis")
+        app.logger.error(str(e))
 
     return "Hello from Python! 3"
 
