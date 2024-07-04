efd35c4d63d210fb631d9dac1af87bd2b97a0318
David Rodríguez <drodriguez@fidelizador.com>
2024-07-04 16:58:51 -0400

    Add logs

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 from flask import Flask
 import redis 
+import sys 
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    print("hola")
+    app.logger.info("hola")
 
     client = redis.StrictRedis(host='redis-cluster', port=6379, decode_responses=True)
     try: 
         client.ping()
-        print("Connected to Redis")
+        app.logger.info("Connected to Redis")
 
     except Exception:
-        print("Error connecting to Redis")
+        app.logger.info("Error connecting to Redis")
 
-    return "Hello from Python!"
+    return "Hello from Python! 3"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
