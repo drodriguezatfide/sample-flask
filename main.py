1d69a0faa1786346cce17b4cbc9b008c6c8e2854
David Rodríguez <drodriguez@fidelizador.com>
2024-07-05 14:12:03 -0400

    Add pulsar code

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask
 import redis 
 import sys 
 import logging
+import pulsar
+import uuid
+
 logging.basicConfig(level=logging.INFO)
 
 
@@ -13,12 +16,30 @@ def hello():
     app.logger.info("hola")
 
     client = redis.StrictRedis(host='10.43.239.42', port=6379, decode_responses=True)
+
+
     try: 
         client.ping()
         app.logger.info("Connected to Redis")
 
     except Exception as e:
         app.logger.info("Error connecting to Redis")
+        app.logger.error(str(e))
+
+
+    try:
+        client = pulsar.Client('pulsar://localhost:6650')
+        topic = 'persistent://fidelizador/relay/test'
+
+        producer = client.create_producer(topic)
+        for i in range(20):
+            text = "{}".format(uuid.uuid4())
+            producer.send(text.encode('utf-8'))
+            logging.info("Message: %s sent to %s", text, topic)
+
+
+    except Exception as e:
+        app.logger.info("Error connecting to Pulsar")
         app.logger.error(str(e))
 
     return "Hello from Python! 3"
